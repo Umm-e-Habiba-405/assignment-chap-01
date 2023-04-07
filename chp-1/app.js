df29fbd8ca8795e4b6277005fb15ff5b26d3b61a
Muzammil-EDF <128902333+Umm-e-Habiba-405@users.noreply.github.com>
2023-04-08 00:38:08 +0500

    .

---
+++ chp-1/app.js
@@ -0,0 +1,12 @@
+// ---------------chp:1------------
+        //    Question no:1
+// alert("Hello user! Welcome to the website");
+            //   Question no:2
+//  alert("Error! Please enter a valid password");
+            //  Question no:3
+//  alert("Welcome to Js land...\n happing coding!");      
+            //  Question no:4
+// alert("Welcome to Js land..");
+// alert("happy coding!");
+        //   Question no:5 
+        // alert("Hello...I can run Js through my web browser's console");
---
+++ chp-2/app.js
@@ -0,0 +1,28 @@
+// ------chp:2
+        //  Question no:1
+        // let username;
+        // Question no:2
+        // let myNmae =("Ummehabiba");
+        // Question no:3
+        // let titledMessage;
+        // titledMessage =("Hello world");
+        // alert("Hello world");
+        // Question no:4
+        // let fullName =("Umm e habiba");
+        // let StudentAge =("18");
+        // let StudentRollNo =("WMA-103779");
+        // alert("Umm e habiba");
+        // alert("18");
+        // alert("WMA-103779");
+        //   Question no:5
+        //  alert("pizza\npizz\npiz\npi\np");
+        //  Question no:6
+        //  let myEmail =("ummehabibah61@gmail.com");
+        //  alert( "My Email address is" +" "+ "ummehabibah61@gmail.com");
+        //    Question no:7
+        // let theBook =("A Smater Way To Learn JavaScript");
+        // alert(" I am trying to learn from the book A Smater \n Way To Learn JavaScript");
+        //    Question no:8
+         document.write("Yah! I can write HTML through Javascript");
+        //   Question no:9
+        // alert("▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ஜ۩۞۩ஜ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬");
